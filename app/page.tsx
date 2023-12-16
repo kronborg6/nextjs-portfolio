3aed9016d6895039fa1e6fc788d3c373d7a61ff7
Kronborg <mkronborg7@gmail.com>
2023-12-17 00:14:02 +0100

    map over static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,40 @@
-const programmingLanguages = ["go", "rust", "javascript", "typescript"];
-const skills = ["git", "scrum"];
-const fremeworks = ["react", "fiber", "gorm", "nextjs"];
-export default function Home({ programmingLanguages, skills, fremeworks }) {
+const programmingLanguages = [
+  { id: 1, name: "go", year: 1 },
+  { id: 2, name: "Rust", year: 2 },
+  { id: 3, name: "C++", year: 20 },
+];
+const skills = [
+  { id: 1, name: "go", year: 1 },
+  { id: 2, name: "Rust", year: 2 },
+  { id: 3, name: "C++", year: 20 },
+];
+const frameworks = [
+  { id: 1, name: "go", year: 1 },
+  { id: 2, name: "Rust", year: 2 },
+  { id: 3, name: "C++", year: 20 },
+];
+
+export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-24">
       <h1 className="text-9xl mb-12">Kronborg</h1>
       <div className="flex flex-col space-y-8">
         {/* Programming Languages */}
         <div className="bg-gray-200 rounded-md overflow-hidden border border-gray-500">
-          {programmingLanguages.map((language, index) => (
-            <h2
-              key={index}
-              className="text-lg font-semibold mb-2 p-4 bg-gray-300 rounded-t-md"
-            >
-              {language}
-            </h2>
-          ))}
+          <h2 className="text-lg font-semibold mb-2 p-4 bg-gray-300 rounded-t-md">
+            programming Languages
+          </h2>
           <div className="grid grid-cols-2 gap-4 p-4">
-            <div className="bg-blue-300 border border-gray-400 p-4 rounded-md">
-              <p>Rust - 5 years</p>
-            </div>
-            <div className="bg-blue-300 border border-gray-400 p-4 rounded-md">
-              <p>Rust - 4 years</p>
-            </div>
-            <div className="bg-blue-300 border border-gray-400 p-4 rounded-md">
-              <p>JavaScript - 20 years</p>
-            </div>
+            {programmingLanguages.map((data) => (
+              <div
+                key={data.id}
+                className="bg-blue-300 border border-gray-400 p-4 rounded-md"
+              >
+                <p>
+                  {data.name} - {data.year} years
+                </p>
+              </div>
+            ))}
             {/* Add more programming languages as needed */}
           </div>
         </div>
@@ -36,15 +45,16 @@ export default function Home({ programmingLanguages, skills, fremeworks }) {
             Skills
           </h2>
           <div className="grid grid-cols-2 gap-4 p-4">
-            <div className="bg-green-300 border border-gray-400 p-4 rounded-md">
-              <p>Skill 1</p>
-            </div>
-            <div className="bg-green-300 border border-gray-400 p-4 rounded-md">
-              <p>Skill 2</p>
-            </div>
-            <div className="bg-green-300 border border-gray-400 p-4 rounded-md">
-              <p>Skill 3</p>
-            </div>
+            {skills.map((data) => (
+              <div
+                key={data.id}
+                className="bg-green-300 border border-gray-400 p-4 rounded-md"
+              >
+                <p>
+                  {data.name} - {data.year} years
+                </p>
+              </div>
+            ))}
             {/* Add more skills as needed */}
           </div>
         </div>
@@ -55,15 +65,16 @@ export default function Home({ programmingLanguages, skills, fremeworks }) {
             Frameworks
           </h2>
           <div className="grid grid-cols-2 gap-4 p-4">
-            <div className="bg-red-300 border border-gray-400 p-4 rounded-md">
-              <p>Framework 1</p>
-            </div>
-            <div className="bg-red-300 border border-gray-400 p-4 rounded-md">
-              <p>Framework 2</p>
-            </div>
-            <div className="bg-red-300 border border-gray-400 p-4 rounded-md">
-              <p>Framework 3</p>
-            </div>
+            {frameworks.map((data) => (
+              <div
+                key={data.id}
+                className="bg-red-300 border border-gray-400 p-4 rounded-md"
+              >
+                <p>
+                  {data.name} - {data.year} years
+                </p>
+              </div>
+            ))}
             {/* Add more frameworks as needed */}
           </div>
         </div>
