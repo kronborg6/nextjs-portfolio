f7844ce68e3f980a7c4e1afed8296749d3df5173
Kronborg <mkronborg7@gmail.com>
2024-02-14 16:30:14 +0100

    start to map it

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -47,7 +47,10 @@ export default function Project() {
                       <div className="flex gap-1">
                         <div className="flex w-full flex-1 flex-wrap items-end gap-4">
                           {data.tags.map((tag) => (
-                            <span className="bg-gray-400 px-2 rounded-md">
+                            <span
+                              key={data.id}
+                              className="bg-gray-400 px-2 rounded-md"
+                            >
                               {tag}
                             </span>
                           ))}
@@ -58,18 +61,13 @@ export default function Project() {
                               <svg
                                 stroke="currentColor"
                                 fill="currentColor"
-                                stroke-width="0"
                                 viewBox="0 0 24 24"
                                 aria-hidden="true"
                                 height="20"
                                 width="20"
                                 xmlns="http://www.w3.org/2000/svg"
                               >
-                                <path
-                                  fill-rule="evenodd"
-                                  d="M2.25 5.25a3 3 0 013-3h13.5a3 3 0 013 3V15a3 3 0 01-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 01-.53 1.28h-9a.75.75 0 01-.53-1.28l.621-.622a2.25 2.25 0 00.659-1.59V18h-3a3 3 0 01-3-3V5.25zm1.5 0v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5z"
-                                  clip-rule="evenodd"
-                                ></path>
+                                <path d="M2.25 5.25a3 3 0 013-3h13.5a3 3 0 013 3V15a3 3 0 01-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 01-.53 1.28h-9a.75.75 0 01-.53-1.28l.621-.622a2.25 2.25 0 00.659-1.59V18h-3a3 3 0 01-3-3V5.25zm1.5 0v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5z"></path>
                               </svg>
                             </a>
                             <a
@@ -80,7 +78,6 @@ export default function Project() {
                               <svg
                                 stroke="currentColor"
                                 fill="currentColor"
-                                stroke-width="0"
                                 role="img"
                                 viewBox="0 0 24 24"
                                 height="20"
